aec4d65435812b32ba23d608123afd032bd5c20a
DDammers-MA <dammersdaniel@gmail.com>
2025-12-08 17:09:16 +0100

    style changes + cart calculations

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, AfterContentChecked {
   product: any[] = [];
   cartTotal = 0;
 
@@ -15,6 +15,11 @@ export class CartComponent implements OnInit {
 
   ngOnInit() {
     this.loadCart();
+    this.calculateTotal();
+  }
+
+  ngAfterContentChecked(): void {
+    this.updateCart();
   }
 
   loadCart() {
@@ -23,10 +28,32 @@ export class CartComponent implements OnInit {
   }
 
   onRemoveItem(productId: number) {
-
-    
     this.product = this.product.filter(item => item.id !== productId);
     localStorage.setItem('cart', JSON.stringify(this.product));
+  }
+
+  calculateTotal() {
+    this.cartTotal = this.product.reduce((total, item) => total + item.price, 0);
+  }
+
+  calculateShipping(): number {
+    return this.cartTotal > 400 ? 0 : 14; 
+  }
+
+  calculateTax(): number {
+    return this.cartTotal * 0.1;
+  }
+
+  calculateGrandTotal(): number {
+
+    return this.cartTotal + this.calculateTax() + this.calculateShipping();
+  }
+
+  updateCart() {
+    this.calculateTotal();
+    this.loadCart();
+    this.calculateTotal();
+    
   }
 
 
